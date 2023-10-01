aad878bb8e71291436fd73d06a16431926815cbe
Lenin Acosta <rkb.lenin@gmail.com>
2023-09-30 23:03:02 -0500

    mapa

---
+++ main.py
@@ -0,0 +1,19 @@
+from queue import Queue
+
+mapa = [
+    ['calle flores', 'av miradero', 'av eloy alfaro', 'UTA 0,3',        'calle ficoa'],
+    ['calle aaa',    'av jjj',      'av sss',         'calle aac',  'calle aba'],
+    ['calle bbb',    'av kkk',      'av ttt',         'calle aad',  'calle aca'],
+    ['calle ccc',    'av lll',      'av uuu',         'calle aae',  'calle ada'],
+    ['calle ddd',    'av mmm',      'av vvv',         'calle aaf',  'calle aea'],
+    ['calle eee',    'av nnn',      'av www',         'calle aag',  'calle afa'],
+    ['calle fff',    'av ooo',      'av xxx',         'calle aah',  'calle aga'],
+    ['calle ggg',    'av ppp',      'av yyy',         'calle aai',  'calle aha'],
+    ['calle hhh',    'av qqq',      'MALL 8,2',         'calle aaj',  'calle ata'],
+    ['calle iii',    'av rrr',      'av aab',         'calle aak',  'calle axa'],
+]
+
+queue = Queue()
+
+while not queue.empty():
+    pass
